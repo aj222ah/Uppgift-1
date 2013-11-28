c1c0a2fa7110e618cfb2ba6f5eee1a66e9636f8b
aj222ah <aj222ah@student.lnu.se>
2013-11-28 11:42:05 +0100

    Update TriangleUnitTest.cs

--- Triangel/kod/c#/TriangleUnitTest.cs
+++ Triangel/kod/c#/TriangleUnitTest.cs
@@ -7,6 +7,13 @@ namespace TriangleUnitTest
     public class TriangleUnitTest
     {
         [TestMethod]
+        public void testConstructorPoint()
+        {
+            Point valPoint = new Point(2.6, 1.8);
+            Assert.IsNotNull(valPoint);
+        }
+        
+        [TestMethod]
         public void testTriangleValid()
         {
             Triangle triVal = new Triangle(2.6, 2.9, 1.8);
@@ -15,39 +22,60 @@ namespace TriangleUnitTest
 
         [TestMethod]
         [ExpectedException(typeof(ArgumentOutOfRangeException))]
-        public void MethodTest()
+        public void testTriangleNegativeValues()
         {
             // Triangel med negativa värden
             var triInval1 = new Triangle(-2.6, -2.9, -1.8);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void testTriangleInequality()
+        {
             // Ogiltig triangel enligt triangelolikheten
             var triInval2 = new Triangle(2, 3, 6);
         }
 
         [TestMethod]
-        public void testTriangleScalene()
+        public void testTriangleIsScalene()
         {
             Triangle triScal = new Triangle(1.5, 2.5, 3.7);
             Assert.IsTrue(triScal.isScalene());
-            Assert.IsFalse(triScal.isIsosceles());
-            Assert.IsFalse(triScal.isEquilateral());
         }
 
         [TestMethod]
-        public void testTriangleEquilateral()
+        public void testTriangleIsEquilateral()
         {
             Triangle triEqui = new Triangle(12.6, 12.6, 12.6);
-            Assert.IsFalse(triEqui.isScalene());
-            Assert.IsFalse(triEqui.isIsosceles());
             Assert.IsTrue(triEqui.isEquilateral());
         }
 
         [TestMethod]
-        public void testTriangleIsosceles()
+        public void testTriangleIsIsosceles()
         {
             Triangle triIso = new Triangle(25.2, 14.3, 25.2);
-            Assert.IsFalse(triIso.isScalene());
             Assert.IsTrue(triIso.isIsosceles());
-            Assert.IsFalse(triIso.isEquilateral());
+        }
+
+        [TestMethod]
+        public void testTriangleIsNotScalene()
+        {
+            Triangle triNotScal = new Triangle(25.2, 14.3, 25.2);
+            Assert.IsFalse(triNotScal.isScalene());
+        }
+
+        [TestMethod]
+        public void testTriangleIsNotEquilateral()
+        {
+            Triangle triNotEqui = new Triangle(1.5, 2.5, 1.5);
+            Assert.IsFalse(triNotEqui.isEquilateral());
+        }
+
+        [TestMethod]
+        public void testTriangleIsNotIsosceles()
+        {
+            Triangle triNotIso = new Triangle(1.5, 2.5, 3.7);
+            Assert.IsFalse(triNotIso.isIsosceles());
         }
     }
 }
